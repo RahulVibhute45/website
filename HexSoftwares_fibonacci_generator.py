a988b530b4d638d9a4cc8fb032118e29b8601782
Rahul Vibhute <rahulvibhute2017@gmail.com>
2025-10-15 15:11:06 +0530

    Update HexSoftwares_fibonacci_generator.py
    added comments for easy understanding

--- HexSoftwares_fibonacci_generator.py
+++ HexSoftwares_fibonacci_generator.py
@@ -1,14 +1,14 @@
 #project name fibbonacci series
 def fib(n):
  i=1
- Ft=0
- St=1
+ Ft=0#first term
+ St=1#second term
  print(f"first term is:{Ft} \nsecond term is:{St} ")
  while i<=n-2 :
-  Tt=Ft+St
+  Tt=Ft+St#third term
   print(f"third term is:{Tt}")
-  Ft=St
-  St=Tt
-  i+=1
-n=int(input("enter number of terms:"))
-fib=fib(n)
+  Ft=St#updating values second term --->first term
+  St=Tt#updating value third term --->second term
+  i+=1#increment value of i by 1
+n=int(input("enter number of terms:"))#taking user input outside function
+fib=fib(n)#calling function
